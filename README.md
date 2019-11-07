1ceb1f8181f17505aafab958e934dced595aa3dd
Robert Antenorcruz <robert.antenorcruzjr@gmail.com>
2019-11-07 16:31:19 -0500

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PDF2OCR
-Converts a non-OCR .pdf into an OCR .pdf
+PowerShell script that converts a non-OCR .pdf into an OCR .pdf
+
+This will convert a non-OCR pdf into an OCR pdf, as well as keep the original file name, plus batch ready.
+
+1. Converts a redenerable .pdf into .tiff files using imageMagick/GhostScript.
+2. Runs tesseract OCR on the .tiff files for readable text.
+3. Converts .tiff files back into .pdfs using tesseract.
+4. Deletes .tiff files.
+
+
+### Instructions
+1. Download dependecies: imageMagick, GhostScript, Tesseract
+2. Set file paths in convert.ps1 to where .pdfs will be located - NOTE: SCRIPT WILL CONVERT ALL .pdfs IN SET DIRECTORY.
+3. Run convert.ps1 script.
---
+++ convert.ps1
@@ -0,0 +1,42 @@
+﻿#Dependencies - imageMagick, GhostScript, tesseract
+
+
+#Comment out the line below to see errors
+$ErrorActionPreference= 'silentlycontinue'
+$counter = 0;
+
+#SET THIS FILEPATH 
+$filepath = "C:\Users\user\Desktop\folder"
+$filetype = "*.pdf"
+$file_count = [System.IO.Directory]::GetFiles("$filepath", "$filetype").Count
+
+#SET THIS FILEPATH
+Set-Location 'C:\Users\user\Desktop\folder'
+
+
+
+Get-ChildItem -Filter '*.pdf' | ForEach-Object {
+  
+    Write-Host "Currently Mogrifying..."
+    mogrify -density 300 -depth 8 -background white -alpha Off -format tiff $_.FullName
+    Write-Host "Done Mogrifying"
+
+        Get-ChildItem -Filter '*.tif' | ForEach-Object {
+        Write-Host "Converting PDF file..."
+        & 'C:\Program Files\Tesseract-OCR\tesseract.exe' -l eng $_.Name ($_.BaseName) pdf;
+        Write-Host "Deleting .tiff"
+        Remove-Item -Path $_.FullName
+        $counter++;
+        Write-Host "Finished Converting PDF file: $counter/$file_count" -ForegroundColor Yellow -BackgroundColor DarkGreen;
+        }
+
+   
+}
+
+
+echo "`nAll done :)"
+
+write-host "Press any key to continue..."
+[void][System.Console]::ReadKey($true)
+
+
